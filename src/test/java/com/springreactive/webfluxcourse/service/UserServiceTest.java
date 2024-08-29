2e3b2520445c0a51846703a60d05c321ea92c8dd
ademir.junior <ademir.junior@apoio.agro.gov.br>
2024-08-29 16:01:05 -0300

    Test findAll, update, delete and notfound

--- src/test/java/com/springreactive/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/com/springreactive/webfluxcourse/service/UserServiceTest.java
@@ -4,11 +4,15 @@ import com.springreactive.webfluxcourse.entity.User;
 import com.springreactive.webfluxcourse.mapper.UserMapper;
 import com.springreactive.webfluxcourse.model.request.UserRequest;
 import com.springreactive.webfluxcourse.respository.UserRepository;
+import com.springreactive.webfluxcourse.service.exception.ObjectNotFoundException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -23,7 +27,7 @@ class UserServiceTest {
     @Mock
     private UserMapper mapper;
 
-    @Mock
+    @InjectMocks
     private UserService service;
 
     @Test
@@ -58,4 +62,63 @@ class UserServiceTest {
         Mockito.verify(repository, times(1)).findById(anyString());
     }
 
+    @Test
+    void testFindALL() {
+        when(repository.findAll()).thenReturn(Flux.just(User.builder().build()));
+
+        Flux<User> result = service.findAll();
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    void testUpdate() {
+        UserRequest request = new UserRequest("Ademir", "ademir@email.com", "Ad123456789@");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("1", request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void testDelete() {
+        User entity = User.builder().build();
+        when(repository.findAndRemove(anyString())).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.delete("1");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).findAndRemove(anyString());
+    }
+
+    @Test
+    void testHandleNotFound() {
+        when(repository.findById(anyString())).thenReturn(Mono.empty());
+
+        try {
+            service.findById("1").block();
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(String.format("Object not found. ID: %s, Type %s", "1", User.class.getSimpleName()), ex.getMessage());
+        }
+    }
 }
