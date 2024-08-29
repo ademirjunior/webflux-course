696ce94c6a86db4307a7fe0b6ea9a71ea2553229
ademir.junior <ademir.junior@apoio.agro.gov.br>
2024-08-29 11:30:04 -0300

    Unity test for save and findbyid

---
+++ src/test/java/com/springreactive/webfluxcourse/service/UserServiceTest.java
@@ -0,0 +1,61 @@
+package com.springreactive.webfluxcourse.service;
+
+import com.springreactive.webfluxcourse.entity.User;
+import com.springreactive.webfluxcourse.mapper.UserMapper;
+import com.springreactive.webfluxcourse.model.request.UserRequest;
+import com.springreactive.webfluxcourse.respository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @Mock
+    private UserService service;
+
+    @Test
+    void testSave() {
+        UserRequest request = new UserRequest("Ademir", "ademir@email.com", "Ad123456789@");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = service.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void testFindById() {
+        when(repository.findById(anyString())).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = service.findById("1");
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).findById(anyString());
+    }
+
+}
